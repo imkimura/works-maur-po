86eae3d5163424eed8cb40e442aac8cbebc19c88
jkssilva <julia.stratelli@gmail.com.br>
2019-11-14 00:12:27 -0300

    logica quase feita

--- hashingTable.py
+++ hashingTable.py
@@ -1,8 +1,7 @@
 class Key:
     def __init__(self, value, prox=None):
-        self.value = value
-        if(prox):
-            self.prox = prox
+        self.value = value        
+        self.prox = None
 
     def getKey(self):
         return self.value
@@ -10,18 +9,41 @@ class Key:
     def setKey(self, value):
         self.value = value
 
+    def getProx(self):
+        return self.prox
+    
+    def __str__(self):
+        return "({}, {})".format(self.value, self.prox)
+
 if __name__ == '__main__':    
     loop = True
     end = [None] * 100
     while(loop):
         op = int(input("Insira uma opção:: "))
         if op == 1:
-            print("Teste")
+            val = int(input("Insira uma chave:: "))
+            if end[val%100] is not None:
+                p = end[val%100]
+                print(p)
+                while(p.getProx()):
+                    p = p.getProx()
+                    print(p)
+                p = Key(val)
+                print(p)
+            else:
+                end[val%100] = Key(val)
         elif op == 2:
             print("Teste")
         elif op == 3:
             for e in end:    
-                print("Teste")
+                if e is not None:
+                    v = str(e.getKey())
+                    p = e
+                    print(str(e.getKey()))
+                    while(p.getProx() is not None):
+                        p = p.getProx()
+                        print(str(e.getKey()))
+                    print(str(e.getKey()))
         else:
             print("Teste")
             loop = False
