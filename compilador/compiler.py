b27296a5aa336cdd6fdc77dbc34e2eda71d82643
elton-tanaka <elton.tanaka@univem.edu.br>
2020-09-29 23:34:44 -0300

    added First method and gramarrules.json

--- compilador/compiler.py
+++ compilador/compiler.py
@@ -5,12 +5,22 @@ class Compiler:
         self.fileName = fileName
         self.countline = 1
         self.listmeans = self.initDictWords()
-        self.stack = []
+        self.syntaxlist = self.initDictSyntax()
+        self.grammarlist = self.initDictGrammar()
+        self.tokenstack = []
     def openFile(self):
         return open(self.fileName, 'r', encoding="utf8")     
 
     def initDictWords(self):
         with open('listmeans.json') as json_file:
+            return json.load(json_file)
+            
+    def initDictSyntax(self):
+        with open('syntaxtable.json') as json_file:
+            return json.load(json_file)
+    
+    def initDictGrammar(self):
+        with open('grammarrules.json') as json_file:
             return json.load(json_file)
 
     def countlines(self):
@@ -20,5 +30,18 @@ class Compiler:
         if searchWord in self.listmeans:
             
             print("{:^5} | {:^6} | {} ".format(self.countline, searchWord, self.listmeans[searchWord]))
-            self.stack.append(searchWord) #unfinished stack method
-            return True   
+            self.tokenstack.append(searchWord) #unfinished stack method
+            return True
+    
+    def First(self, symbol):
+        if symbol in self.grammarlist:
+            for rule in self.grammarlist[symbol]:
+                firstsymbol = list(rule)[0]
+
+                if firstsymbol in self.grammarlist:
+                    return First(firstsymbol)
+                else:
+                    #unfinished again    
+
+
+
