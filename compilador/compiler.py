f3a32235b60a52e0ce65eec02653bf94e897fbfc
elton-tanaka <elton.tanaka@univem.edu.br>
2020-10-10 22:23:49 -0300

    added non-recursive predictive parsing

--- compilador/compiler.py
+++ compilador/compiler.py
@@ -5,22 +5,12 @@ class Compiler:
         self.fileName = fileName
         self.countline = 1
         self.listmeans = self.initDictWords()
-        self.syntaxlist = self.initDictSyntax()
-        self.grammarlist = self.initDictGrammar()
         self.tokenstack = []
     def openFile(self):
         return open(self.fileName, 'r', encoding="utf8")     
 
     def initDictWords(self):
         with open('listmeans.json') as json_file:
-            return json.load(json_file)
-            
-    def initDictSyntax(self):
-        with open('syntaxtable.json') as json_file:
-            return json.load(json_file)
-    
-    def initDictGrammar(self):
-        with open('grammarrules.json') as json_file:
             return json.load(json_file)
 
     def countlines(self):
@@ -32,13 +22,3 @@ class Compiler:
             print("{:^5} | {:^6} | {} ".format(self.countline, searchWord, self.listmeans[searchWord]))
             self.tokenstack.append(searchWord) #unfinished stack method
             return True
-    
-    def First(self, symbol):
-        if symbol in self.grammarlist:
-            for rule in self.grammarlist[symbol]:
-                firstsymbol = list(rule)[0]
-
-                if firstsymbol in self.grammarlist:
-                    return First(firstsymbol)
-                else:
-                    #unfinished again    
---
+++ compilador/nonrecursive.py
@@ -0,0 +1,71 @@
+import json
+
+def initDictGrammar():
+    with open('grammarrules.json') as json_file:
+        return json.load(json_file)
+
+def initDictSyntax():
+    with open('syntaxtable.json') as json_file:
+        return json.load(json_file)
+
+syntaxlist = initDictSyntax()
+terminals = initDictGrammar()['terminals']
+nonterminals = initDictGrammar()['nonterminals']
+stack = ["$", "E"]
+
+buffer = input("Cadeia: ")
+buffer += ' $'
+buffer = buffer.split(" ")
+print(syntaxlist)
+
+print('Stack: ',stack)
+
+def firstInBuffer():
+    return buffer[0]
+
+def firstInStack(): 
+    return stack[len(stack)-1]
+
+while len(stack) != 1: #while stack is not empty
+
+    if firstInStack() in terminals:
+
+        print('Buffer: ', buffer)
+        print('Action: -----')
+        print('')
+
+        if firstInBuffer() == firstInStack():
+            buffer.pop(0)
+            stack.pop()
+        else:
+            #Error, token in buffer is not the same as in stack
+            print("ERROR 1")
+            break
+        print('Stack: ',stack)
+
+    elif firstInStack() in nonterminals: 
+        action = syntaxlist[firstInStack()][firstInBuffer()]
+        
+        print('Buffer: ', buffer)
+        print('Action: ', firstInStack(), '->', action)
+        print('')
+
+        if action == 'ERROR':
+            print('ERROR 2')
+            break
+        stack.pop()
+
+        for symbol in reversed(action):
+            if symbol != 'LAMBDA': #char 'λ' was having problems, replaced with 'LAMBDA'
+                stack.append(symbol)
+
+            print('Stack: ',stack)
+        
+    else:
+        #Error, token wasnt recognized 
+        print("ERROR 3")
+        break
+
+if buffer[0] == '$':
+    print('Buffer: ', buffer)
+    print('Regra: SUCESSO')
